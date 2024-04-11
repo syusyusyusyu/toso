7f0d69819fea197721e71a4dd6b466c166285491
吉田柊 <C2375@oic.jp>
2024-04-11 17:47:25 +0900

    slide追加

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import data from "./data.json";
 
 import MainImage from "./components/MainImage/MainImage";
 import Card from "./components/Card/Card";
+import EnhancedSwipeableViews from 'react-swipeable-views';
 import Footer from "./components/Footer/Footer";
 
 const App = () => {
@@ -23,6 +24,11 @@ const App = () => {
           );
         })}
       </div>
+      <EnhancedSwipeableViews enableMouseEvents>
+      <div className="slide">あああああ</div>
+      <div className="slide">いいいいい</div>
+      <div className="slide">ううううう</div>
+      </EnhancedSwipeableViews>
       <Footer />
     </div>
   );
