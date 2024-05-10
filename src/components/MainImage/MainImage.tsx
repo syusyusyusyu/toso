1d1f85e1426f0c3f0c3af256d7803ca43178d09e
shu_ragi <123170586+syusyusyusyu@users.noreply.github.com>
2024-05-10 12:56:54 +0900

    サイズ変更

--- src/components/MainImage/MainImage.tsx
+++ src/components/MainImage/MainImage.tsx
@@ -6,7 +6,7 @@ const MainImage = () => {
         <section>
             <div className={styles.main_image}>
                 <h1 className={styles.title}>
-                <img src={process.env.PUBLIC_URL + '/images/logo1outline.png'} alt="Logo" />
+                <img src={process.env.PUBLIC_URL + '/images/logo1outline.png'} alt="Logo" width="200"/>
                 </h1>
                 <div className={styles.pic_down}></div>
             </div>
