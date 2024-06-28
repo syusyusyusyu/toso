5171284083d2a43d3c7bd79ccf3ecf7c19297afd
吉田柊 <C2375@oic.jp>
2024-06-28 10:49:10 +0900

    a

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,21 +1,18 @@
 import React from "react"
 import styles from "./Footer.module.css"
-import { FaGithub, FaTwitter } from "react-icons/fa"
+import { FaTwitter } from "react-icons/fa"
 
 const Footer = () => {
     return(
         <footer className={styles.footer}>
             <small>
             <div>
-                <a href="https://twitter.com/OicItc" target="blank">
+                <a href="https://twitter.com/hiiraagii" target="blank">
                     <FaTwitter className={styles.icon} />
-                </a>
-                <a href="https://github.com/ITCreate" target="blank">
-                    <FaGithub className={styles.icon} />
                 </a>
             </div>
             <p>
-                &copy;2024 ITクリエイト部
+                &copy;2024 Shu Yoshida 
                 </p>
             </small>
         </footer>
--- src/components/MainImage/MainImage.tsx
+++ src/components/MainImage/MainImage.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import styles from"./MainImage.module.css"
 
 const MainImage = () => {
-    return(
+    return (
         <section>
             <div className={styles.main_image}>
                 <h1 className={styles.title}>
-                <img className={styles.logo} src={process.env.PUBLIC_URL + '/images/logo1outline.png'} alt="Logo" />
+                    <p　style={{textAlign:'left'}}>
+                        名前：吉田 柊（よしだ しゅう）<br />
+                        年齢：22歳（専門学生）<br />
+                        出身：奈良県
+                    </p>
                 </h1>
                 <div className={styles.pic_down}></div>
             </div>
         </section>
-    )
+    );
 }
 
 export default MainImage
