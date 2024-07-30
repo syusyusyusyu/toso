38c46ac8af9ca9f8b6a3331169131ed4a5473099
吉田柊 <C2375@oic.jp>
2024-07-30 22:44:08 +0900

    a

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import './Menu.css';
+import IMG_3608 from '../images/IMG_3608.png';
+import IMG_3609 from '../images/IMG_3609.png';
+import IMG_3733 from '../images/IMG_3733.jpg';
 
 const Menu = () => {
   const data = [
     {
       title: "逃走中との出会い",
       text: "5歳の頃に放送されたジャンプ！〇〇中を見て釘付け！以降どんな用事があろうと欠かさずリアルタイム視聴",
-      imagePath: process.env.PUBLIC_URL + "/images/IMG_3608.png"
+      imagePath: IMG_3608
     },
     {
       title: "支え",
       text: "幼少期からの難病で何もできなかった人生を支えてくれたのが逃走中の存在です。回復した今逃走成功を果たして一つの成功体験を掴みたい！",
-      imagePath: process.env.PUBLIC_URL + "/images/IMG_3609.png"
+      imagePath: IMG_3609
     },
     {
       title: "3000時間プレイ",
       text: "『逃走中 史上最強のハンターたちからにげきれ』は発売から遊び続け、そのプレイ時間は12年間で3000時間を突破！！",
-      imagePath: process.env.PUBLIC_URL + "/images/IMG_3733.jpg"
+      imagePath: IMG_3733
     }
   ];
 
